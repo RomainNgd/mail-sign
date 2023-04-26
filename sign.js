93e556eb38dbddcfd1f64cdcec8ffd8d74cb2315
Romain NIGOND <romain@waybox.local>
2023-04-26 17:10:45 +0200

    finission

---
+++ sign.js
@@ -0,0 +1,61 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+    document.getElementById('signature').insertAdjacentHTML('beforeend', '<table style=" width:40%; padding: 1% ;background-image: linear-gradient(to right bottom, #000000, #060606, #0c0c0c, #111111, #151515, #161617, #171818, #181919, #171818, #161717, #141515, #131414);">\n' +
+        '    <tbody>\n' +
+        '        <tr style="">\n' +
+        '            <td style="border-right: #c6ab5b solid 2px;text-align: center; padding: 2% ;width: 40%">\n' +
+        '                <div>\n' +
+        '                    <span id="firstname" style="color: #c6ab5b; font-size: large; text-transform: uppercase">nigond</span>\n' +
+        '                    <span id="lastname" style="color: #ffffff; font-size: large; text-transform: uppercase">romain</span>\n' +
+        '                </div>\n' +
+        '                <p style="color: #ffffff; font-size: large; text-transform: uppercase">développeur web</p>\n' +
+        '                <img style="width: 50%" src="img/logo-waybox%20blanc.png" id="logo-waybox" alt="waybox logo">\n' +
+        '                <a href="https://www.linkedin.com/in/romain-nigond/" style="display: flex; align-items: center; justify-content: center; margin-top: 5%" id="personal-linkedin">\n' +
+        '                    <img style="margin-right: 5%; width: 10%" src="img/logo-linkedin-gold.png" alt="">\n' +
+        '                    <span style="color: #ffffff; font-size: small;"> linkedin/romain-nigond </span>\n' +
+        '                </a>\n' +
+        '            </td>\n' +
+        '            <td style="width: 20%; padding: 2%; text-align: center">\n' +
+        '                    <img style="border: solid #c6ab5b 2px; border-radius: 10000000px; padding: 20%; width: 100%" src="img/cube-waybox-blanc.png" alt="">\n' +
+        '            </td>\n' +
+        '            <td style="border-left: #c6ab5b solid 2px;text-align: left; padding: 2%; width: 40%">\n' +
+        '                <table width="100%" style="border-collapse: collapse; border-spacing: 0">\n' +
+        '                    <tbody>\n' +
+        '                        <tr style="width: 100%">\n' +
+        '                            <td style="background-color: #c6ab5b; padding: 5%; width: 20%; vertical-align: middle; horiz-align: center">\n' +
+        '                                <img src="img/logo-linkedin-black.png" alt="" style="width: 100%">\n' +
+        '                            </td>\n' +
+        '                            <td>\n' +
+        '                                <a style="color: #ffffff; margin-left: 10%" href="https://www.linkedin.com/mwlite/in/waybox-officiel-56458996">waybox-officiel</a>\n' +
+        '                            </td>\n' +
+        '                        </tr>\n' +
+        '                        <tr style="width: 100%">\n' +
+        '                            <td style="background-color: #c6ab5b; padding: 5%; width: 20%; vertical-align: middle; horiz-align: center">\n' +
+        '                                <img src="img/accueil.png" alt="" style="width: 100%">\n' +
+        '                            </td>\n' +
+        '                            <td style="margin-left: 20%">\n' +
+        '                                <p style="color: #ffffff; margin-left: 10%">15 All. du chargement, 59650 Villeneuve d\'Ascq</p>\n' +
+        '                            </td>\n' +
+        '                        </tr>\n' +
+        '                        <tr style="width: 100%;">\n' +
+        '                            <td style="background-color: #c6ab5b; padding: 5%; width: 20%; vertical-align: middle; horiz-align: center">\n' +
+        '                                <img src="img/globe.png" alt="" style="width: 100%">\n' +
+        '                            </td>\n' +
+        '                            <td style="margin-left: 20%">\n' +
+        '                                <a style="color: #ffffff; margin-left: 10%" href="https://www.waybox.fr">waybox.fr</a>\n' +
+        '                            </td>\n' +
+        '                        </tr>\n' +
+        '                        <tr style="width: 100%">\n' +
+        '                            <td style="background-color: #c6ab5b; padding: 5%; width: 20%; vertical-align: middle; horiz-align: center">\n' +
+        '                                <img src="img/phone.png" alt="" style="width: 100%;">\n' +
+        '                            </td>\n' +
+        '                            <td style="margin-left: 20%">\n' +
+        '                                <p style="color: #ffffff; margin-left: 10%" >0 826 468 400</p>\n' +
+        '                            </td>\n' +
+        '                        </tr>\n' +
+        '                    </tbody>\n' +
+        '                </table>\n' +
+        '            </td>\n' +
+        '        </tr>\n' +
+        '    </tbody>\n' +
+        '</table>')
+})
